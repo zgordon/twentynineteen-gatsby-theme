74d7dcb4427b9112125cf6115af4a3abf590372b
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2019-03-04 14:45:29 +0530

    add autoprefixer

--- packages/twentynineteen-gatsby-theme/gatsby-config.js
+++ packages/twentynineteen-gatsby-theme/gatsby-config.js
@@ -1,3 +1,17 @@
+const autoprefixer = require("autoprefixer");
+
 module.exports = {
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-postcss"]
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: "gatsby-plugin-postcss",
+      options: {
+        postCssPlugins: [
+          autoprefixer({
+            browsers: ["last 2 versions"]
+          })
+        ]
+      }
+    }
+  ]
 };
