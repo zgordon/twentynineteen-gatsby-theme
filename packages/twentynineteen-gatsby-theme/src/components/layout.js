0abc11a57ad8b93ccfccd3bf3680d3492abcbd75
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2019-03-04 14:33:20 +0530

    Comment out two styles that broke the build and replace with %

--- packages/twentynineteen-gatsby-theme/src/components/layout.js
+++ packages/twentynineteen-gatsby-theme/src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { StaticQuery, graphql } from "gatsby";
+// import { StaticQuery, graphql } from "gatsby";
 import Header from "./header";
 import Footer from "./footer";
 import "../style.scss";
