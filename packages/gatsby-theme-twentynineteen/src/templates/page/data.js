97efba3050edb950ed8115ddcaf61524709e776d
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2019-03-07 10:24:52 +0530

    createPages working

---
+++ packages/gatsby-theme-twentynineteen/src/templates/page/data.js
@@ -0,0 +1,10 @@
+const PageTemplateFragment = `
+    fragment PageTemplateFragment on Page { 
+      id
+      pageId
+      title
+      content
+    }
+`
+
+module.exports.PageTemplateFragment = PageTemplateFragment
