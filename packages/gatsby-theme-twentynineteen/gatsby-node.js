096f7090edf6596609f8b801193419067c295ac8
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2019-03-09 17:38:14 +0530

    createCategories done

--- packages/gatsby-theme-twentynineteen/gatsby-node.js
+++ packages/gatsby-theme-twentynineteen/gatsby-node.js
@@ -1,9 +1,11 @@
 const createPosts = require(`./create/createPosts`)
 const createPages = require(`./create/createPages`)
+const createCategories = require(`./create/createCategories`)
 const createTags = require(`./create/createTags`)
 
 exports.createPages = async ({ actions }) => {
   await createPosts({ actions })
   await createPages({ actions })
+  await createCategories({ actions })
   await createTags({ actions })
 }
---
+++ packages/gatsby-theme-twentynineteen/src/templates/categories/single.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Layout from "../../components/layout"
+import PostEntry from "../posts/post-entry"
+
+const SingleCategory = props => {
+  const {
+    pageContext: { name, posts },
+  } = props
+  return (
+    <Layout classNames="archive">
+      <header className="page-header">
+        <h1 className="page-title">
+          Category Archives: <span className="page-description">{name}</span>
+        </h1>
+      </header>
+
+      {posts.nodes &&
+        posts.nodes.map(post => {
+          return <PostEntry key={post.postId} post={post} />
+        })}
+    </Layout>
+  )
+}
+
+export default SingleCategory
