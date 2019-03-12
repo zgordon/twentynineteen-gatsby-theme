c3dc0ca30aeaf97ef46b341648de1fdd37d7e078
hussain <hussainibnthaj@gmail.com>
2019-03-12 18:14:00 +0530

    Add categories widget with StaticQuery

--- packages/gatsby-theme-twentynineteen/src/templates/categories/single.js
+++ packages/gatsby-theme-twentynineteen/src/templates/categories/single.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../../components/layout"
 import PostEntry from "../../components/post-entry"
 import SEO from "../../components/seo"
