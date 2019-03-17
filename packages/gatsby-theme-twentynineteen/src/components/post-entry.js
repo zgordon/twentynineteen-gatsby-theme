6b30c68697a5a4f7ffea0aecacb0364bcf883f9c
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2019-03-17 11:27:41 +0530

    when excerpt does not exist

--- packages/gatsby-theme-twentynineteen/src/components/post-entry.js
+++ packages/gatsby-theme-twentynineteen/src/components/post-entry.js
@@ -3,8 +3,32 @@ import { Link } from "gatsby"
 import PostFooterMeta from "./post-footer-meta"
 
 const PostEntry = ({
-  post: { id, postId, title, excerpt, uri, author, date, categories, tags },
+  post: {
+    id,
+    postId,
+    title,
+    excerpt,
+    content,
+    uri,
+    author,
+    date,
+    categories,
+    tags,
+  },
 }) => {
+  // getting the excerpt to a variable
+  let excerptText = excerpt
+
+  // if excerpt does not exist
+  if (!excerptText) {
+    // getting the first 240 characters off content
+    excerptText = content.substr(0, 240)
+
+    // so that a word is not chopped off halfway
+    excerptText = content
+      .substr(0, Math.min(excerptText.length, excerptText.lastIndexOf(" ")))
+      .concat("...")
+  }
   return (
     <article
       className="post type-post status-publish format-standard hentry entry"
@@ -20,7 +44,7 @@ const PostEntry = ({
         </h2>
       </header>
       <div className="entry-content">
-        <div dangerouslySetInnerHTML={{ __html: excerpt }} />
+        <div dangerouslySetInnerHTML={{ __html: excerptText }} />
         <Link to={`/blog/${uri}`} className="more-link">
           Continue reading
           <span className="screen-reader-text">{title}</span>
